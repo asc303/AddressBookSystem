bca87b43ff7653766f13fc815e826f99f3e8d830
asc303 <chauhansam94@gmail.com>
2021-12-12 15:31:21 +0530

    [ashutosh] add:Unique Contact

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 
 namespace AddressBookSystem
 {
@@ -13,7 +12,7 @@ namespace AddressBookSystem
             while (flag)
             {
                 Console.WriteLine("Welcome to the Address Book Program");
-                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contacts \n 4. Exit");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contacts \n 4. Delete Contact \n 5. Add Multiple Contact \n 6. Add Unique Contact \n 7. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -51,10 +50,39 @@ namespace AddressBookSystem
                         Console.WriteLine("Enter the Contact Name to  Edit: ");
                         string name = Console.ReadLine();
                         addressBook.EditContact(name);
-                        Console.WriteLine("Contact is Edited Sucsessfully");
+                        Console.WriteLine("Contact is Edited Successfully");
                         addressBook.Display();
                         break;
                     case 4:
+                        Console.WriteLine("Enter the Contact Name to deleted : ");
+                        string detail = Console.ReadLine();
+                        addressBook.DeleteContact(detail);
+                        addressBook.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter the Contact Information to Add, in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        contact = new Contact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        addressBook.AddContact(contact);
+                        Console.WriteLine("Contact is Saved");
+                        addressBook.Display();
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter the Unique Contact to be added: ");
+                        string unique = Console.ReadLine();
+                        addressBook.AddUniqueContact(unique);
+                        addressBook.DisplayUniqueContacts();
+                        break;
+                    case 7:
                         flag = false;
                         break;
                 }
